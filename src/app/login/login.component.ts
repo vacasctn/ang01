1f6f48d9a0929db4e91de28739b7a0583cf3fb20
vacasctn <vacasctn@gmail.com>
2020-09-20 02:35:31 -0400

    ngIf

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,13 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   username = 'Add user name' 
   password = ''
+  errorMessage = 'Invalid credentials'
+  invalidLogin = false
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
   handleLogin(){
-    console.log(this.username); 
+    //console.log(this.username); 
+    if(this.username==="waqas" && this.password==='dummy'){
+      this.invalidLogin = false
+
+    } else {
+      this.invalidLogin = true
+    }
   }
 
 }
